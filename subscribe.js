78bb004e932f13c532263e27984eb1dec71b6ac6
I-Dalgleish <99255280+I-Dalgleish@users.noreply.github.com>
2022-04-05 18:42:17 +0200

    Update subscribe.js
    email function

--- subscribe.js
+++ subscribe.js
@@ -1,2 +1,11 @@
-  
- 
+ body onLoad="redirect()"
+
+script language="JavaScript"
+
+function redirect() 
+
+      var email = "xyz@something.com"
+      var mailto_link = 'mailto:' + email
+      window = window.open(mailto_link, 'emailWindow')
+      if (window && window.open && !window.closed)         
+          window.close()
